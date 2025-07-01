abf702ad2ac773f3c0209eb6277e3cffae4386c5
Robert Sokolov <sokolov.r123@gmail.com>
2025-07-01 15:56:07 +0200

    Added back DB migrate call

--- Backend/SkillForge/SkillForge/Program.cs
+++ Backend/SkillForge/SkillForge/Program.cs
@@ -124,6 +124,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    using IServiceScope scope = app.Services.CreateScope();
+    AppDbContext db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+
     app.UseExceptionHandler("/Dashboard/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
