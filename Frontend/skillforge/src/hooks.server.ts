cf66d9ce429600e71253e56e3f95fe35e44d5d89
Robert Sokolov <sokolov.r123@gmail.com>
2025-07-02 13:11:01 +0200

    Reapply "Debug headers"
    This reverts commit dde2026f11748c84db27e671e311b8d2d9708e3e.

--- Frontend/skillforge/src/hooks.server.ts
+++ Frontend/skillforge/src/hooks.server.ts
@@ -3,6 +3,7 @@ import type { HandleFetch } from "@sveltejs/kit";
 
 export const handleFetch: HandleFetch = async ({ request, fetch }) => {
     console.error('[SSR FETCH]', request.url);
+    console.error('Headers:', request.headers);
 
     if (request.url.startsWith(PUBLIC_BASE_URL)) {
         // Workaround: https://github.com/sveltejs/kit/issues/6608
