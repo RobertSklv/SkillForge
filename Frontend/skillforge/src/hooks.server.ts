21b0b9d2fef4599bbd461a962439d2ca3b04e858
Robert Sokolov <sokolov.r123@gmail.com>
2025-07-02 14:54:00 +0200

    Revert "Debug response headers"
    This reverts commit 00dec396bb371fb47585250a1d5ec2574b70d624.

--- Frontend/skillforge/src/hooks.server.ts
+++ Frontend/skillforge/src/hooks.server.ts
@@ -3,18 +3,12 @@ import type { HandleFetch } from "@sveltejs/kit";
 
 export const handleFetch: HandleFetch = async ({ request, fetch }) => {
     console.error('[SSR FETCH]', request.url);
+    console.error('Headers:', request.headers);
 
     if (request.url.startsWith(PUBLIC_BASE_URL)) {
         // Workaround: https://github.com/sveltejs/kit/issues/6608
         request.headers.set('origin', PUBLIC_BASE_URL);
     }
 
-    let response = fetch(request);
-
-    response
-        .then(r => {
-            console.error('Response headers:', r.headers);
-        });
-
-    return response;
+    return fetch(request);
 };
