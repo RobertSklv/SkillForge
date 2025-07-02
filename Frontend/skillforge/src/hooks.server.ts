10747dc661c7102432312a24b86d4a4ee9bf6136
Robert Sokolov <sokolov.r123@gmail.com>
2025-07-02 13:10:59 +0200

    Reapply "Changed log type to error"
    This reverts commit 97aa9d43602798783700f5d51c6fe9682d374fd6.

--- Frontend/skillforge/src/hooks.server.ts
+++ Frontend/skillforge/src/hooks.server.ts
@@ -2,8 +2,8 @@ import { PUBLIC_BASE_URL } from "$env/static/public";
 import type { HandleFetch } from "@sveltejs/kit";
 
 export const handleFetch: HandleFetch = async ({ request, fetch }) => {
-    console.log('[SSR FETCH]', request.url);
-    
+    console.error('[SSR FETCH]', request.url);
+
     if (request.url.startsWith(PUBLIC_BASE_URL)) {
         // Workaround: https://github.com/sveltejs/kit/issues/6608
         request.headers.set('origin', PUBLIC_BASE_URL);
