987cd7ee5740b0c832e67eb19dccd1bace254af8
Robert Sokolov <sokolov.r123@gmail.com>
2025-07-22 18:07:24 +0200

    Installed react-helmet and added meta tags

--- Frontend/skillforge-react/src/pages/join/Join.tsx
+++ Frontend/skillforge-react/src/pages/join/Join.tsx
@@ -1,15 +1,21 @@
 import { LoginForm } from '@components/login-form/LoginForm';
 import { RegisterForm } from '@components/register-form/RegisterForm';
+import { Helmet } from 'react-helmet';
 
-export function Join () {
-  return (
-    <div className="row">
-        <div className="col-12 col-lg-6">
-            <LoginForm />
-        </div>
-        <div className="col-12 col-lg-6">
-            <RegisterForm />
-        </div>
-    </div>
-  );
+export function Join() {
+	return (
+		<div className="row">
+			<Helmet>
+				<title>SkillForge | Join us</title>
+				<meta name="description" content="Register or Login to SkillForge" />
+				<meta name="robots" content="noindex,nofollow" />
+			</Helmet>
+			<div className="col-12 col-lg-6">
+				<LoginForm />
+			</div>
+			<div className="col-12 col-lg-6">
+				<RegisterForm />
+			</div>
+		</div>
+	);
 }
