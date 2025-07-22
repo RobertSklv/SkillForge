f807d1535e3f35339db14b290db691ee62cc79db
Robert Sokolov <sokolov.r123@gmail.com>
2025-07-22 17:52:18 +0200

    Removed import for non-existing stylesheet

--- Frontend/skillforge-react/src/main.tsx
+++ Frontend/skillforge-react/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import 'bootswatch/dist/vapor/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import App from './App.tsx';
